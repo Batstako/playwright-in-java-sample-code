ef6a29c35f111eeec728313fedca6fe26b83b84b
StanislavTsvetkashki <stanislav.tsvetkashki@gmail.com>
2025-02-25 17:30:46 +0200

    first tests

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -1,9 +1,15 @@
 package com.serenitydojo.playwright;
 
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.SelectOption;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class ASimplePlaywrightTest {
 
@@ -51,6 +57,68 @@ public class ASimplePlaywrightTest {
         int matchingProductCount = page.locator(".card-title").count();
 
         Assertions.assertTrue(matchingProductCount > 0);
+    }
+
+    @DisplayName("Interacting with text fields")
+    @Nested
+    class WhenInteractingWithTextField {
+        @BeforeEach
+        void openContactPage(){ page.navigate("https://practicesoftwaretesting.com/contact");}
+
+        @DisplayName("Complete the Form")
+        @Test
+        void completeForm(){
+            var firstNameField = page.getByLabel("First name");
+            var lastNameField = page.getByLabel("Last name");
+            var emailNameField = page.getByLabel("Email");
+            var messageNameField = page.getByLabel("Message");
+            var subjectField = page.getByLabel("Subject");
+
+            firstNameField.fill("Sahra-jane");
+            lastNameField.fill("Smith");
+            emailNameField.fill("sahra@tino.com");
+            messageNameField.fill("Hello, world");
+            subjectField.selectOption("Warranty");
+//            subjectField.selectOption(new SelectOption().setIndex(5));
+
+            assertThat(firstNameField).hasValue("Sahra-jane");
+            assertThat(lastNameField).hasValue("Smith");
+            assertThat(emailNameField).hasValue("sahra@tino.com");
+            assertThat(messageNameField).hasValue("Hello, world");
+            assertThat(subjectField).hasValue("warranty");
+
+        }
+
+        @DisplayName("Mandatory fields")
+        @ParameterizedTest
+        @ValueSource(strings = {"First name", "Last name", "Email", "Message"})
+        void mandatoryFields(String fieldName){
+            var firstNameField = page.getByLabel("First name");
+            var lastNameField = page.getByLabel("Last name");
+            var emailNameField = page.getByLabel("Email");
+            var messageNameField = page.getByLabel("Message");
+//            var subjectField = page.getByLabel("Subject");
+            var sendButton = page.getByText("Send");
+
+
+            //Fill in the field values
+            firstNameField.fill("Sahra-jane");
+            lastNameField.fill("Smith");
+            emailNameField.fill("sahra@tino.com");
+            messageNameField.fill("Hello, world");
+//            subjectField.selectOption("Warranty");
+
+            //Clear one of the fields
+            page.getByLabel(fieldName).clear();
+
+            sendButton.click();
+
+            //Check the error message for that field
+
+
+            var errorMessage = page.getByRole(AriaRole.ALERT).getByText(fieldName + " is required");
+//            assertThat(errorMessage).isVisible();
+        }
     }
 
 }
